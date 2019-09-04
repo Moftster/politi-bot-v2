50f02c99cba7a36ff66421e0750c551596fdf2fa
David Moftakhar <david@sellmysnaps.com>
2019-09-04 11:33:33 +0100

    navbar collapse centered

--- assets/partials/header.php
+++ assets/partials/header.php
@@ -42,7 +42,8 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-light">
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+
+	<button class="navbar-toggler ml-auto mx-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
   <div class="collapse navbar-collapse listitems justify-content-center" id="navbarNav">
