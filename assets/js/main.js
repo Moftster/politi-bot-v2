3ecec46dfc55296cc26d9ba20f1784458002a8fe
David Moftakhar <david@sellmysnaps.com>
2019-09-07 14:09:50 +0100

    Alert message if all fields not completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,16 +22,18 @@ function play(element) {
 
 var form = document.getElementById('theForm');
 
-form.addEventListener('submit', testSubmit)
-
+form.addEventListener('submit', testSubmit);
 
 function testSubmit(e){
   e.preventDefault();
   var userName = document.getElementById('userName').value;
   var request = document.getElementById('request').value;
+  if(userName == 0 || request == 0) {
+    alert("All fields must be completed");
+  } else {
   var openWinToSend = window.open(`mailto:david@sellmysnaps.com?subject=Politi-bot request&body=Hi,%0D%0A%0D%0AI\'d like to request ${request} as a Politi-bot.%0D%0A%0D%0AThanks,%0D%0A%0D%0A${userName}`);
-
   setTimeout(function() {openWinToSend.close()}, 4);
+}
 }
 
 //
