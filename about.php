d9a84ed7fd80d6aa8fcad8095905c6ec323f69e4
David Moftakhar <david@sellmysnaps.com>
2019-09-10 19:38:11 +0100

    About page complete.

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php include('assets/partials/header.php'); ?>
+
+<div id="main">
+
+    <div class="inner">
+
+      <div class="container">
+
+                  <div class="row">
+                      <div class="col-md-8">
+                        <h2 class="form-header">Welcome to Politi-bot</h2>
+                        <h6 class="robo-text questions">Do you want to hear a politician's stock response one more time?</h6>
+                        <h6 class="robo-text questions">Was there a political slogan you just can't wait to hear again?</h6>
+                        <h6 class="robo-text questions">Do you want world leaders to answer your burning questions?</h6>
+                        <p class="robo-text">If you answered yes to any of the above then Politi-bot.com is for you! Peruse the site and hear more from the political leader of your choosing past or present.</p>
+                      </div>
+                      <div class="col-md-4">
+                        <img class="img-fluid" src="images/robot-about.jpg">
+                      </div>
+                  </div>
+        </div>
+        </div>
+      </div>
+
+<script src="assets/js/contact.js"></script>
+
+<?php include('assets/partials/footer.php'); ?>
